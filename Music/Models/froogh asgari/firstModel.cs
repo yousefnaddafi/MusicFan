aa9f8864c5682ad4e3ef0df805994ad95bc475e5
forough_asgari <77478727+AStudent-art-maker2@users.noreply.github.com>
2021-01-19 00:02:54 +0330

    gcgh

---
+++ Music/Models/froogh asgari/firstModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Music.Models.froogh_asgari
+{
+    public class firstModel
+    {
+        public string Name { get; set; }
+    }
+}
