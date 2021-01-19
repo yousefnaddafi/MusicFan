312175511f86f4528d1334718ccdb679a694ba74
forough_asgari <77478727+AStudent-art-maker2@users.noreply.github.com>
2021-01-19 16:07:32 +0330

    h

--- Music/Controllers/infoAlbumController.cs
+++ Music/Controllers/infoAlbumController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Music.HttpService;
+using Music.Models.froogh_asgari;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,16 @@ namespace Music.Controllers
     [ApiController]
     public class infoAlbumController : ControllerBase
     {
+        private readonly MusicReport musicReport;
+        public infoAlbumController(MusicReport musicReport)
+        {
+            this.musicReport = musicReport;
+        }
+
+        [HttpGet]
+        public Mode2 GetAlbumDetail([FromQuery] string ArtistName)
+        {
+            return musicReport.GetAlbumInfo(ArtistName);
+        }
     }
 }
