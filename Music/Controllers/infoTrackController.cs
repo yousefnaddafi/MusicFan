655a011ba103c4d5b1db36ff72dce2b6040fb225
forough_asgari <77478727+AStudent-art-maker2@users.noreply.github.com>
2021-01-19 14:27:18 +0330

    xf

--- Music/Controllers/infoTrackController.cs
+++ Music/Controllers/infoTrackController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Music.HttpService;
+using Music.Models.froogh_asgari;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,12 @@ namespace Music.Controllers
     [ApiController]
     public class infoTrackController : ControllerBase
     {
+        private readonly MusicReport musicReport;
+
+        public infoTrackController(MusicReport musicReport)
+        {
+            this.musicReport = musicReport;
+        }
+ 
     }
 }
